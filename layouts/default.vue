8f6e310b6379ee3af2797008d315c81310e4a3c8
Andrew Foster <andrewconwayfoster@gmail.com>
2023-12-18 19:17:41 -0700

    overly complicated title function

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <Header />
     <ClientOnly>
-      <TitleSection>{{ $route.name }}</TitleSection>
+      <TitleSection>{{ pageTitle }}</TitleSection>
     </ClientOnly>
 
     <slot />
@@ -20,6 +20,27 @@ export default {
     Header,
     Footer,
     TitleSection,
-  }
+  },
+  computed: {
+    pageTitle() {
+      // one off replacements
+      const replacements = {
+        "index": "home",
+        "services-": "",
+        "previous-engagements": "What We've Done",
+      };
+      let words = this.$route.name;
+      Object.keys(replacements).forEach((replacement) => {
+        words = words.replace(replacement, replacements[replacement]);
+      });
+      // format title
+      const wordsSplit = words.replace(/-+/gi, " ").split(" ");
+      for (let i = 0; i < wordsSplit.length; i++) {
+        wordsSplit[i] =
+          wordsSplit[i][0].toUpperCase() + wordsSplit[i].substr(1);
+      }
+      return wordsSplit.join(" ");
+    },
+  },
 };
 </script>
