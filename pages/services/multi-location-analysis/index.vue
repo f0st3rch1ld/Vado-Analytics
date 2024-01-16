43d917c9d4538394dadbda9229008f44057e8499
Andrew Foster <andrewconwayfoster@gmail.com>
2024-01-15 22:42:33 -0700

    multi-location and new location pages

--- pages/services/multi-location-analysis/index.vue
+++ pages/services/multi-location-analysis/index.vue
@@ -5,8 +5,39 @@
     </Head>
     <TitleSection>Multi-Location Analysis</TitleSection>
     <Content>
-      <div class="column is-full">
-        <p>Page Content</p>
+      <div class="column">
+        <h3 class="title is-size-2">Multi-Location Analysis</h3>
+        <p>
+          Using 3rd party GPS data, learn who is visiting your location, who is
+          visiting your competitors’ locations, and who to market to in order to
+          increase your marketshare.<br /><br />Not only can Vado tell you who
+          and where your customers live, we can tell you all about your
+          competitors. Build a comprehensive marketing strategy that will
+          deliver the optimal ROI for your spend.
+        </p>
+      </div>
+      <div class="column">
+        <figure class="image">
+          <img src="~/assets/images/webp/multi-location-analysis-1.webp" />
+        </figure>
+      </div>
+    </Content>
+    <Content sectionBackground="#eff1fe">
+      <div class="column">
+        <figure class="image">
+          <img src="~/assets/images/webp/multi-location-analysis-2.webp" />
+        </figure>
+      </div>
+      <div class="column">
+        <h3 class="title is-size-2">Marketing Strategy</h3>
+        <p>
+          You are not the only one who defines your marketplace. Competition
+          determines how far people are willing to come to you, how they prefer
+          you compared to your competitors, and the approach to capture
+          marketshare.<br /><br />
+          Know how you stack up against your competitors, where to gain
+          marketshare, and the optimal marketing plans to grow your business.
+        </p>
       </div>
     </Content>
   </div>
--- pages/services/new-location-placement/index.vue
+++ pages/services/new-location-placement/index.vue
@@ -5,8 +5,37 @@
     </Head>
     <TitleSection>New Location Placement</TitleSection>
     <Content>
-      <div class="column is-full">
-        <p>Page Content</p>
+      <div class="column">
+        <h3 class="title is-size-2">New Location Placement</h3>
+        <p>
+          Stop guessing on where to open your location. Using 3rd party GPS data
+          we can tell you the optimal area to open you new location based on who
+          goes to retail store location database, your competitors’ stores, and
+          the areas that are least served in the marketplace.<br /><br />Instead
+          of guessing one of the most important aspects of your business, make
+          an informed decision of where your new location should be. In
+          addition, use our analysis to determine the appropriate marketing plan
+          to ensure that your new locations is a success and what we have done
+          now!
+        </p>
+      </div>
+      <div class="column">
+        <figure class="image">
+          <img src="~/assets/images/webp/new-location-placement-1.webp" />
+        </figure>
+      </div>
+    </Content>
+    <Content sectionBackground="#eff1fe">
+      <div class="column">
+        <figure class="image">
+          <img src="~/assets/images/webp/multi-location-analysis-2.webp" />
+        </figure>
+      </div>
+      <div class="column">
+        <h3 class="title is-size-2">Strategic Location Selection</h3>
+        <p>
+          Our approach is to be with you from start to finish of your location expansion. We recommend where your location should be and will work with your real estate team to determine the appropriate sites.<br /><br />But that is only half of the effort, we will then work with your marketing departments to implement a marketing strategy that ensures results. If we have 1st party transaction detail we are also able to provided an expected revenue forecast for the first 24 months of the new store, so you are able to plan accordingly with best data analytics companies in world.
+        </p>
       </div>
     </Content>
   </div>
