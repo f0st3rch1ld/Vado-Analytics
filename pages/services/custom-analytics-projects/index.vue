14ca620c0c9d680d03f7d99b2e52f7e4eef0c03f
Andrew Foster <andrewconwayfoster@gmail.com>
2024-01-15 22:46:00 -0700

    custom analytics projects

--- pages/services/custom-analytics-projects/index.vue
+++ pages/services/custom-analytics-projects/index.vue
@@ -5,8 +5,38 @@
     </Head>
     <TitleSection>Custom Analytics Projects</TitleSection>
     <Content>
-      <div class="column is-full">
-        <p>Page Content</p>
+      <div class="column">
+        <h3 class="title is-size-2">Custom Analytics Projects</h3>
+        <p>
+          Our most frequent engagements. Combine the power of 3rd party GPS
+          sources with your 1st party sales data to get a truly refined sense of
+          the marketplace. We layer your sales data on top of your competitors’
+          traffic to get a sense of where, who, and how you should be marketing
+          to maximize ROI to best data analytics companies in world.
+        </p>
+      </div>
+      <div class="column">
+        <figure class="image">
+          <img src="~/assets/images/webp/custom-analytics-projects-1.webp" />
+        </figure>
+      </div>
+    </Content>
+    <Content sectionBackground="#eff1fe">
+      <div class="column">
+        <figure class="image">
+          <img src="~/assets/images/webp/custom-analytics-projects-2.webp" />
+        </figure>
+      </div>
+      <div class="column">
+        <h3 class="title is-size-2">Partnership Approach</h3>
+        <p>
+          Our partnership approach ensures that we will assist you with not only
+          your initial campaign deployment, but a custom machine learning model
+          that continuously enhances your marketing efforts to optimize ROI with
+          retail store location database.<br /><br />We partner with you after
+          deployment to assist your marketing teams on enhancements to your
+          marketing campaigns to ensure success.
+        </p>
       </div>
     </Content>
   </div>
