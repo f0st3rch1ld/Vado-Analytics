013975fb27d9dbf249b0514753294664a79757c0
Andrew Foster <andrewconwayfoster@gmail.com>
2024-01-18 18:45:10 -0700

    temporarily disabled previous-engagments page

--- pages/previous-engagements.vue
+++ pages/previous-engagements.vue
@@ -11,6 +11,8 @@
 </template>
 
 <script>
+// NOTE: I left this page here for now. It is temporarily redirected to the home page. To disable the redirect, open the nuxt.config.ts file in the root directory, and remove the redirect placed on the previous-engagements page from the route rules object.
+
 import Content from "~/components/common/content";
 import TitleSection from "~/components/common/page/titleSection";
 
